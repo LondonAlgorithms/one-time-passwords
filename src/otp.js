eeeaa53681639fe78e7b5843421baa4f4c90266e
Callum Macrae <callum@samknows.com>
2017-11-22 18:17:30 +0000

    removed answers

--- src/otp.js
+++ src/otp.js
@@ -1,48 +1,22 @@
-const tokenLength = 10;
-
 function HOTP(secret) {
-  this.secret = secret;
 }
 
 HOTP.prototype.generate = function (counter) {
-  return hmacSHA1(this.secret, counter).slice(-tokenLength);
 };
 
 HOTP.prototype.validate = function (token, counter, float = 50) {
-  for (let i = counter; i < counter + float; i++) {
-    if (this.generate(i) === token) {
-      return true;
-    }
-  }
-
-  return false;
 };
 
 
 function TOTP(secret) {
-  this.hotp = new HOTP(secret);
 }
 
 TOTP.prototype.generate = function () {
-  return this.hotp.generate(now());
 };
 
 TOTP.prototype.validate = function (token, float = 180) {
-  const counter = now();
-
-  for (let i = counter; i > counter - float; i--) {
-    if (this.hotp.generate(i) === token) {
-      return true;
-    }
-  }
-
-  return false;
 };
 
-
-function now() {
-  return Math.floor(Date.now() / 1000);
-}
 
 
 
